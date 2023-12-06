d20b7d7494230bf46ed9ad00ec638535a2a9ff9b
rodrigoxaviersantos <rodrigoxaviersantoss@gmail.com>
2023-12-06 15:19:04 -0300

    Add a dependency

---
+++ ApiCatalogo/Context/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Context
+{
+        public class ApiDbContext : DbContext
+        {
+            public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+            { }
+
+            public DbSet<Categoria> Categorias { get; set; }
+            public DbSet<Produto> Produtos { get; set; }
+
+        }
+
+}
